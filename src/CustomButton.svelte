7aaf266d32a315344a6002798de51cc0435ed478
Maurialva <mauri.mdp87@gmail.com>
2021-05-06 23:22:20 -0300

    cambiado boton

--- src/CustomButton.svelte
+++ src/CustomButton.svelte
@@ -9,26 +9,26 @@
 		background-color: rgb(90, 29, 29);
 		border-color: #693030;
 		font-size: 1.7rem;
-		background-image: linear-gradient(90deg, rgb(65, 9, 9) 0%, rgb(158, 80, 80) 56%, rgb(54, 12, 12) 100%);
+	
 		background-position: 100%;
 		background-size: 400%;
+		transition: background 900ms ease-in-out;
 		text-align: center;
 		color: rgb(0, 0, 0); 
 		font-size: 22px; 
-		line-height: 19px; 
+		line-height: 15px; 
 		padding: 12px; 
 		border-radius: 50px; 
-		 
+		background-image: linear-gradient(45deg,rgb(65, 9, 9) 0%, rgb(158, 80, 80) 56%, rgb(54, 12, 12) 100%); 
 		vertical-align:top ;
 		border: 2px solid rgb(47, 6, 6); 
 		display: inline-block;
 		cursor: pointer;
-		transition: background 900ms ease-in-out;
 	}
 	button:hover {
-		background-position: 100%;
+		background-position: 0;
 		color: rgb(87, 12, 12);
-		background-image: linear-gradient(30deg,rgb(0, 0, 0) 0%, rgb(37, 26, 26) 56%, rgb(39, 4, 4) 100%); 
+		background-image: linear-gradient(43deg,rgb(0, 0, 0) 0%, rgb(37, 26, 26) 56%, rgb(39, 4, 4) 100%); 
 		color: rgb(255, 255, 255); 
 	}
 </style>
